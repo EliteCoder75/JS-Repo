2997f5d4514bf4e8a43fae729d4ddc74a6aa626a
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-10-30 16:24:11 +0100

    js exercices solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+const reverseString = function (s){
+    return s.split("").reverse().join("");
+}
+;
 
 // Do not edit below this line
 module.exports = reverseString;
