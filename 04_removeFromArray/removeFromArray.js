1d0ea26673b0c005c120611a67713162313e1d0c
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-11-02 11:41:13 +0100

    correction of the fourth exercice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+
+
+
+const removeFromArray = function (array, ...rest) { 
+    let itemsToRemove = rest.length > 0 ? rest : [];
+    return array.filter(function(x) { 
+        return itemsToRemove.indexOf(x) < 0;
+    });
 };
 
-// Do not edit below this line
+
+console.log(removeFromArray([1,3], 1));
+
 module.exports = removeFromArray;
+
+
+
+
