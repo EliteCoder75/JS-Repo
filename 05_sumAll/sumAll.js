8b0bb0e323920bcdcfedef79dac743ffcbcd6f84
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-11-02 12:11:33 +0100

    fifth exercice finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+function sumAll (a, b) {
+    let sum = 0;
 
+    
+    if ( (a<0) || (b<0) || !isNumber(a) || !isNumber(b) || !Number.isInteger(a) || !Number.isInteger(b) ){
+        return "ERROR"
+    } 
+    else if (a<b){
+        for(let i=a; i<=b; i++){
+            sum += i;
+        }}
+    else if (a>b) {
+        for(let i=a; i>=b; i--){
+            sum += i;
+        }
+    }   
+
+    return sum;
 };
 
+function isNumber(value) {
+    return typeof value === 'number';
+}
+
+console.log(sumAll(1,3));
 // Do not edit below this line
 module.exports = sumAll;
